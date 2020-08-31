57c5dfa0c3e412fb3056e29d209bfbca6622fb81
Cheezus <rcheeze1337@gmail.com>
2020-08-30 23:43:01 -0400

    Remove goto, reorganize some things

--- lua/autorun/pencheck.lua
+++ lua/autorun/pencheck.lua
@@ -38,7 +38,7 @@ if SERVER then
             end
 
             --Check whether unfrozen entities are penetrating eachother, and if they are, maintain a table of them per-player
-            if ent:IsValid() and ent:CPPIGetOwner() and ent:CPPIGetOwner():IsValid() and ent:GetPhysicsObject():IsValid() and ent:GetPhysicsObject():IsMoveable() and not ent:IsPlayerHolding() then
+            if ent:IsValid() and ent:CPPIGetOwner():IsValid() and ent:GetPhysicsObject():IsValid() and ent:GetPhysicsObject():IsMoveable() then
                 if not ent:CPPIGetOwner().penetrating then
                     ent:CPPIGetOwner().penetrating = {}
                 end
@@ -48,62 +48,66 @@ if SERVER then
         end
 
         --Check how many entities are penetrating eachother per-player, if this exceeds a set value, freeze the penetrating entities
+        --freezeCount represents the number of times a player's props have been frozen due to penetration issues
+        --If a player's props are frozen due to penetration issues 3 times in a short period, lock them out of everything for 60 seconds
         for _, ply in pairs(player.GetAll()) do
-            if ply.penetrating then
+            if not ply.penetrating then continue end
+            local count = table.Count(ply.penetrating)
+
+            if ply.freezeLockout and count > 0 then
+                ply:FreezeAll()
+                continue
+            end
+
+            if count > 3 then
+                if not ply.freezeCount then
+                    ply.freezeCount = 0
+                end
+
+                ply.freezeCount = ply.freezeCount + 1
+                ply:SendMsg(Color(255, 127, 0), "[AntiLag] ", Color(255, 0, 0), count .. " of your entities were frozen due to penetrating eachother")
+
                 for ent, _ in pairs(ply.penetrating) do
                     if not ent:IsValid() then
                         ply.penetrating[ent] = nil
+                        continue
                     end
+
+                    ent:GetPhysicsObject():EnableMotion(false)
+
+                    if ent:GetClass() == "prop_vehicle_jeep" or ent:GetClass() == "prop_vehicle_jalopy" then
+                        ent:Remove()
+                        ply:SendMsg(Color(255, 127, 0), "[AntiLag] ", Color(255, 0, 0), "Cannot freeze " .. ent:GetClass() .. ", removing")
+                    end
+
+                    ply.penetrating[ent] = nil
                 end
 
-                local count = table.Count(ply.penetrating)
-
-                if ply.freezeLockout and count > 0 then
+                if ply.freezeCount > 2 then
                     ply:FreezeAll()
-                    goto cont
-                end
+                    ply.freezeLockout = true
+                    ply:SetNWBool("PCLockout", true)
+                    ply:SendMsg(Color(255, 127, 0), "[AntiLag] ", Color(255, 0, 0), "Stop doing that - all of your entities have been frozen and you have been temporarily restricted from further spawning...")
+                    BroadcastMsg(Color(255, 0, 255), "[Server] ", Color(255, 0, 0), ply:Nick() .. " has been temporarily restricted due to repeatedly creating penetrating physics objects")
 
-                if count > 3 then
-                    if not ply.freezeCount then
+                    timer.Simple(60, function()
+                        ply.freezeLockout = false
+                        ply:SetNWBool("PCLockout", false)
                         ply.freezeCount = 0
-                    end
-
-                    ply.freezeCount = ply.freezeCount + 1
-                    ply:SendMsg(Color(255, 127, 0), "[AntiLag] ", Color(255, 0, 0), count .. " of your entities were frozen due to penetrating eachother")
-
-                    for ent, _ in pairs(ply.penetrating) do
-                        ent:GetPhysicsObject():EnableMotion(false)
-
-                        if ent:GetClass() == "prop_vehicle_jeep" or ent:GetClass() == "prop_vehicle_jalopy" then
-                            ent:Remove()
-                            ply:SendMsg(Color(255, 127, 0), "[AntiLag] ", Color(255, 0, 0), "Cannot freeze " .. ent:GetClass() .. ", removing")
-                        end
-
-                        ply.penetrating[ent] = nil
-                    end
-
-                    --If a player's entities are frozen more than twice for penetration, lock them out of everything for 60 seconds and freeze all of their owned entities
-                    if ply.freezeCount > 2 then
-                        ply:FreezeAll()
-                        ply.freezeLockout = true
-                        ply:SetNWBool("PCLockout", true)
-                        ply:SendMsg(Color(255, 127, 0), "[AntiLag] ", Color(255, 0, 0), "Stop doing that - all of your entities have been frozen and you have been temporarily restricted from further spawning...")
-                        BroadcastMsg(Color(255, 0, 255), "[Server] ", Color(255, 0, 0), ply:Nick() .. " has been temporarily restricted due to repeatedly creating penetrating physics objects")
-
-                        timer.Simple(60, function()
-                            ply.freezeLockout = false
-                            ply:SetNWBool("PCLockout", false)
-                            ply.freezeCount = 0
-                        end)
-                    end
+                    end)
                 end
-
-                ::cont::
             end
         end
     end)
 
-    --While players are locked out due to too many penetration events, they can't do any of the following:
+    timer.Create("PenCheck::Cooldown", 30, 0, function()
+        for _, ply in pairs(player.GetAll()) do
+            if ply.freezeCount and ply.freezeCount > 0 then
+                ply.freezeCount = ply.freezeCount - 1
+            end
+        end
+    end)
+
     hook.Add("CanTool", "PenCheck::Lockout", function(ply)
         if ply.freezeLockout then return false end
     end)
