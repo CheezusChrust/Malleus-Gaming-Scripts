6929416dea9d0e56a9b5a55564c4f22c337dbe2d
Matt <rcheeze1337@gmail.com>
2022-04-28 23:10:46 -0400

    de uglied some stuff, display unknown owner entities

--- lua/autorun/propinfo.lua
+++ lua/autorun/propinfo.lua
@@ -17,8 +17,6 @@ local function getNWVectorPrecise(ent, key, fallback)
 end
 
 if SERVER then
-    hook.Remove("Think", "PI::PropData")
-
     timer.Create("PI::PropData", 0.1, 0, function()
         for _, ply in pairs(player.GetAll()) do
             local ent = ply:GetEyeTrace().Entity
@@ -48,48 +46,73 @@ else
         hook.Remove("HUDPaint", "NADMOD.HUDPaint") --Sorry NADmod!
     end)
 
+    local ownedByWorld = {
+        "prop_door",
+        "prop_dynamic",
+        "func_",
+        "C_BaseEntity"
+    }
+
+    local function getEntityOwner(E)
+        if not E:IsValid() then return end
+        if not E:CPPIGetOwner() then
+            for _, v in pairs(ownedByWorld) do
+                if string.find(E:GetClass(), v) then return "World" end
+            end
+            return "Unknown"
+        end
+        return E:CPPIGetOwner():Nick()
+    end
+
     hook.Add("HUDPaint", "PI::PropInfo", function()
-        surface.SetFont("ChatFont")
+        local aimEntity = LocalPlayer():GetEyeTrace().Entity
+        if not aimEntity:IsValid() then return end
+        if aimEntity:GetClass() == "player" then return end
+        local xPos = tonumber(GetConVar("propinfo_x"):GetString()) or 0.01
+        local yPos = tonumber(GetConVar("propinfo_y"):GetString()) or 0.01
+        local mode = GetConVar("propinfo_mode"):GetInt()
+        local mode2 = mode - (getEntityOwner(aimEntity) == "World" and 1 or 0)
+        local justify = GetConVar("propinfo_justify"):GetInt()
+
+        if mode < 1 then return end
+
         local inertia = getNWVectorPrecise(LocalPlayer(), "PI::Inertia", Vector())
         inertia = inertia:Length() > 4294967295 and Vector() or inertia
         local mass = math.Round(LocalPlayer():GetNWInt("PI::Mass"))
         local physprop = LocalPlayer():GetNWString("PI::PhysProp")
-        local xPos = tonumber(GetConVar("propinfo_x"):GetString()) or 0.01
-        local yPos = tonumber(GetConVar("propinfo_y"):GetString()) or 0.01
-        local mode = GetConVar("propinfo_mode"):GetInt()
-        local justify = GetConVar("propinfo_justify"):GetInt()
-        if mode < 1 then return end
-        local aimEntity = LocalPlayer():GetEyeTrace().Entity
-        if not aimEntity:IsValid() then return end
-        if not IsValid(aimEntity:CPPIGetOwner()) then return end
+
         local split = string.Split(aimEntity:GetModel(), "/")
         local modelStr = split[#split] .. " [" .. aimEntity:EntIndex() .. "]"
         local classStr = "Class: " .. aimEntity:GetClass()
-        local ownerStr = "Owner: " .. aimEntity:CPPIGetOwner():Nick()
+        local ownerStr = "Owner: " .. getEntityOwner(aimEntity)
         local angleStr = "Angle: " .. matrixToString(aimEntity:GetAngles(), 3)
         local inertiaStr = "Inertia: " .. (inertia ~= Vector() and matrixToString(inertia, 2) or "Unknown")
+        local physpropStr = "Physprop: " .. (physprop and physprop or "Unknown")
+
+        surface.SetFont("ChatFont")
         local w0 = select(1, surface.GetTextSize(ownerStr))
         local w1 = select(1, surface.GetTextSize(modelStr))
-        local w2 = mode > 1 and select(1, surface.GetTextSize(inertiaStr)) or 0
+        local w2 = mode2 > 1 and select(1, surface.GetTextSize(inertiaStr)) or 0
         local w3 = select(1, surface.GetTextSize(classStr))
-        local w4 = mode > 1 and select(1, surface.GetTextSize(angleStr)) or 0
-        local width = math.max(w0 + 8, w1 + 8, w2 + 8, w3 + 8, w4 + 8)
+        local w4 = mode2 > 1 and select(1, surface.GetTextSize(angleStr)) or 0
+        local w5 = mode2 > 1 and select(1, surface.GetTextSize(physpropStr)) or 0
+        local width = math.max(w0 + 8, w1 + 8, w2 + 8, w3 + 8, w4 + 8, w5 + 8)
 
         if justify < 1 then
-            draw.RoundedBox(5, x * xPos, y * yPos, width, 62 + (mode - 1) * 80, Color(0, 0, 0, 127))
+            draw.RoundedBox(5, x * xPos, y * yPos, width, 62 + (mode2 - 1) * 80, Color(0, 0, 0, 127))
         else
-            draw.RoundedBox(5, (x * xPos) - width, y * yPos, width, 62 + (mode - 1) * 80, Color(0, 0, 0, 127))
+            draw.RoundedBox(5, (x * xPos) - width, y * yPos, width, 62 + (mode2 - 1) * 80, Color(0, 0, 0, 127))
         end
 
         draw.SimpleText(ownerStr, "ChatFont", x * (xPos + 0.002 - justify * 0.004), y * (yPos + 0.002), Color(255, 255, 255), justify * 2, 0)
         draw.SimpleText(modelStr, "ChatFont", x * (xPos + 0.002 - justify * 0.004), y * (yPos + 0.002) + 20, Color(255, 255, 255), justify * 2, 0)
         draw.SimpleText(classStr, "ChatFont", x * (xPos + 0.002 - justify * 0.004), y * (yPos + 0.002) + 40, Color(255, 255, 255), justify * 2, 0)
 
-        if GetConVar("propinfo_mode"):GetInt() > 1 then
+        if mode2 > 1 then
             draw.SimpleText(angleStr, "ChatFont", x * (xPos + 0.002 - justify * 0.004), y * (yPos + 0.002) + 60, Color(255, 255, 255), justify * 2, 0)
             draw.SimpleText(inertiaStr, "ChatFont", x * (xPos + 0.002 - justify * 0.004), y * (yPos + 0.002) + 80, Color(255, 255, 255), justify * 2, 0)
             draw.SimpleText("Mass: " .. (mass and (mass .. "kg") or "Unknown"), "ChatFont", x * (xPos + 0.002 - justify * 0.004), y * (yPos + 0.002) + 100, Color(255, 255, 255), justify * 2, 0)
-            draw.SimpleText("Physprop: " .. (physprop and physprop or "Unknown"), "ChatFont", x * (xPos + 0.002 - justify * 0.004), y * (yPos + 0.002) + 120, Color(255, 255, 255), justify * 2, 0)
+            draw.SimpleText(physpropStr, "ChatFont", x * (xPos + 0.002 - justify * 0.004), y * (yPos + 0.002) + 120, Color(255, 255, 255), justify * 2, 0)
         end
     end)
 end
